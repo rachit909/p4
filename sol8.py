6c04a9c70ecabdda86d9b641ce854263541f2782
eecs388 <krachit@umich.edu>
2023-03-29 19:33:16 +0000

    Automatic commit by test script for krachit vkaushik

--- sol8.py
+++ sol8.py
@@ -18,8 +18,8 @@ padding = b'A' * 112
 
 data = padding + bytes.fromhex("080496d7")
 
-sys.stdout.buffer.write(len(padding + (0x080496d7).to_bytes(4,'little')).to_bytes(4, 'little') 
-                        + padding + (0x080496d7).to_bytes(4,'little') + 
+sys.stdout.buffer.write(len(b'A' * 112 + (0x080496d7).to_bytes(4,'little')).to_bytes(4, 'little') 
+                        + b'A' * 112 + (0x080496d7).to_bytes(4,'little') + 
                         hmac.new(bytes.fromhex("fd35144ef39207ab0882ae7a1d9db00b4f68778d715b6999bb53d149190388d4"), padding + (0x080496d7).to_bytes(4,'little'), hashlib.sha256).digest())
 
 
