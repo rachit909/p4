dba3bd886acf4cd54934fd594c3d7ca0c54c5b11
eecs388 <krachit@umich.edu>
2023-03-29 19:32:42 +0000

    Automatic commit by test script for krachit vkaushik

--- sol8.py
+++ sol8.py
@@ -1,7 +1,6 @@
 import sys
 import hashlib
 import hmac
-import base64
 
 
 """
@@ -19,10 +18,9 @@ padding = b'A' * 112
 
 data = padding + bytes.fromhex("080496d7")
 
-data2 = padding + (0x080496d7).to_bytes(4,'little')
-
-mac_addy = hmac.new(bytes.fromhex("fd35144ef39207ab0882ae7a1d9db00b4f68778d715b6999bb53d149190388d4"), data2, hashlib.sha256)
-sys.stdout.buffer.write(len(data2).to_bytes(4, 'little') + data2 + mac_addy.digest())
+sys.stdout.buffer.write(len(padding + (0x080496d7).to_bytes(4,'little')).to_bytes(4, 'little') 
+                        + padding + (0x080496d7).to_bytes(4,'little') + 
+                        hmac.new(bytes.fromhex("fd35144ef39207ab0882ae7a1d9db00b4f68778d715b6999bb53d149190388d4"), padding + (0x080496d7).to_bytes(4,'little'), hashlib.sha256).digest())
 
 
 
